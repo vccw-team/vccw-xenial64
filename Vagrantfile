0012409441d98ffbb733b288988bc3a7e55c828b
miya0001 <miya@wpist.me>
2016-09-04 00:07:14 +0900

    some fix

--- Vagrantfile
+++ Vagrantfile
@@ -7,6 +7,8 @@ Vagrant.configure("2") do |config|
   config.vm.box_check_update = false
   config.vm.network "private_network", ip: "192.168.33.10"
 
+  config.vm.synced_folder ".", "/vagrant", :mount_options => ['dmode=755', 'fmode=644']
+
   config.vm.provision "shell", inline: "sudo apt-get dist-upgrade -y && sudo apt-get install -y python-simplejson"
 
   config.vm.provision "ansible" do |ansible|
---
+++ clear.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -ex
+
+apt-get clean
+
+rm -f /etc/ssh/ssh_host_*
+cd /var/log
+find /var/log/ -type f -name '*.log' -exec cp /dev/null {} \;
+cp /dev/null /var/log/syslog
+
+yes | cp /dev/null /root/.bash_history
+yes | cp /dev/null /home/vagrant/.bash_history
+
+ln -s -f /dev/null /etc/udev/rules.d/70-persistent-net.rules
+
+curl -L https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub > /home/vagrant/.ssh/authorized_keys
+
+chown -R vagrant:vagrant /home/vagrant/.ssh
+chmod 755 /home/vagrant/.ssh
+chmod 644 /home/vagrant/.ssh/authorized_keys
+
+/etc/init.d/vboxadd setup
+
+history -c
+
