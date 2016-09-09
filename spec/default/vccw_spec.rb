3a4b70f4f25231b46f9de2a98503ed8ec9df4be0
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2016-09-10 02:49:44 +0900

    active mod_rewirte. add test.

--- spec/default/vccw_spec.rb
+++ spec/default/vccw_spec.rb
@@ -9,6 +9,11 @@ describe service('apache2') do
   it { should be_running }
 end
 
+describe command('apache2ctl -M') do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should match /rewrite_module/ }
+end
+
 describe port(80) do
   it { should be_listening }
 end
